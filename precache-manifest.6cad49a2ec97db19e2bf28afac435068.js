f6c188109eb505e9896ff10e5186990edf2d0597
Lashonda Miller <lashondamiller007@gmail.com>
2020-08-09 18:19:44 -0400

    Updates

---
+++ precache-manifest.6cad49a2ec97db19e2bf28afac435068.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99630a3f016d1657f39a7fe3c94bbc5d",
+    "url": "/noodles/index.html"
+  },
+  {
+    "revision": "03ca4cfbec640e06e762",
+    "url": "/noodles/static/css/main.863f997b.chunk.css"
+  },
+  {
+    "revision": "c2162168ade1c2feb57b",
+    "url": "/noodles/static/js/2.ff082def.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/noodles/static/js/2.ff082def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ca4cfbec640e06e762",
+    "url": "/noodles/static/js/main.19dae6ff.chunk.js"
+  },
+  {
+    "revision": "00fa80cd5d5b74c996d9",
+    "url": "/noodles/static/js/runtime-main.2932be6e.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/noodles/static/media/pokeball.f10bf078.png"
+  }
+]);
